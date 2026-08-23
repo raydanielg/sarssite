75fc673a6ca41579c92b0d973d5b532767d4a519
raydanielg <airezra2+1@gmail.com>
2026-08-23 21:22:20 +0300

    commit

--- app/Http/Controllers/Landing/ResultsController.php
+++ app/Http/Controllers/Landing/ResultsController.php
@@ -242,14 +242,7 @@ class ResultsController extends Controller
             }
         }
 
-        $regionSummaries = ResultSummary::whereHas('resultTitle', function ($q) use ($examName, $yearData, $region) {
-            $q->where('name', $examName)
-              ->where('year_id', $yearData->id)
-              ->where('region_id', $region->id)
-              ->whereNull('district_id');
-        })->get();
-
-        return view('landing.results.exam_districts', compact('examName', 'examSlug', 'yearData', 'region', 'districts', 'regionSummaries'));
+        return view('landing.results.exam_districts', compact('examName', 'examSlug', 'yearData', 'region', 'districts'));
     }
 
     public function showExamFinal(Request $request, $examSlug, $year, $region_slug, $district_slug)
